8dd360409acd4b6c2edf7b9e2fe7dbbf5fef6337
Havi-Dev-Py <dvszlvr@gmail.com>
2025-09-30 21:08:22 -0400

    Create exe07.py

---
+++ python_course_huawei/cap05/exe07.py
@@ -0,0 +1,15 @@
+"""
+Escreva um programa que permaneça em laço lendo quantidades (números inteiros) de produtos
+vendidos. O laço termina quando for digitado zero ou um valor negativo. Ao término do laço exiba na
+tela a soma de todas as quantidades digitadas (se for digitado um negativo para sair do laço ele não
+deve afetar o total).
+"""
+soma = 0
+while True:
+    num = int(input('Número: '))
+    if num > 0:
+        soma += num
+    else:
+        print('Fim do programa')
+        break
+print(f'A soma de todos os valores é: {soma}')
